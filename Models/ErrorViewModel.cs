bfdfad0dd35f2b3fe2bd0210649444dfbd0cfc7a
Rinkle Cleverdesign <rinkle.del@gmail.com>
2025-07-30 09:56:02 +0530

    sdsad

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkeyWebNewTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
